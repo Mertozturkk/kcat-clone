2d35ea9406007c94c1604c60cfeffa3d920e2c7b
Mert Öztürk <mert.ozturk@teleskop.app>
2023-11-22 10:01:52 +0300

    parse method updated for colorize

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/fatih/color"
 	"gopkg.in/yaml.v3"
 	"os"
 )
@@ -27,10 +28,26 @@ func main() {
 
 	content := v.Content[0]
 
+	colorizeKeys(content)
+
 	// encode yaml
 	enc := yaml.NewEncoder(os.Stdout)
 	enc.SetIndent(2)
 	if err := enc.Encode(content); err != nil {
 		panic(err)
 	}
+
+}
+func colorizeKeys(node *yaml.Node) {
+
+	for i, child := range node.Content {
+		if node.Kind == yaml.SequenceNode && child.Kind == yaml.ScalarNode {
+			continue
+		}
+		if i%2 == 0 && child.Value != "" {
+			child.Value = color.WhiteString(child.Value)
+		}
+		colorizeKeys(child)
+	}
+
 }
